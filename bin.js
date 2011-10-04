6b925fd5df5e366009d6061f689ada19773fa237
Bram Stein <bst@one.com>
2011-10-04 11:11:36 +0200

    Use optimist to parse options.

--- bin.js
+++ bin.js
@@ -1,23 +1,50 @@
 #!/usr/bin/env node
 
-var Replicator = require("./main");
+var Replicator = require("./main"),
+    optimist = require('optimist'),
+    argv = optimist
+        .usage('usage: $0 [options] to from')
+        .demand(2)
+        .wrap(80)
+        .options('c', {
+            alias: 'continuous',
+            boolean: true,
+            description: 'Trigger continuous replication.'
+        })
+        .options('s', {
+            alias: 'since',
+            number: true,
+            description: 'Replication seq identifier to start replicating at.'
+        })
+        .options('h', {
+            alias: 'help',
+            boolean: true
+        })
+        .argv;
 
-function replicate (from, to, cb) {
-  if (typeof from === 'object') var options = from
-  else {
-    var options = {from:from, to:to}
-  }
-  var rep = new Replicator(options)
-  rep.push(cb)
-  return rep
+
+if (argv.help) {
+    optimist.showHelp();
+} else {
+    var from = argv._[0],
+        to = argv._[1],
+        replicator = new Replicator({
+            from: from,
+            to: to
+        });
+
+    replicator.on('failed', function (obj) {
+        console.warn('Failed to replicate "' + obj.id + '" at rev: "' + obj.rev + '".');
+    });
+
+    replicator.on('pushed', function (obj) {
+        console.log('Replicated "' + obj.id + '" at rev: "' + obj.rev + '".');
+    });
+
+    if (!argv.continuous) {
+        replicator.push(function (results) {
+            console.log('replication complete');
+        });
+    }
 }
-
-var args = process.argv.slice(0);
-
-// shift off node and script name
-args.shift(); args.shift();
-
-if(args.length < 2) throw "syntax: replicate http://admin:pass@somecouch/sourcedb http://admin:pass@somecouch/destinationdb"
-
-replicate(args[0], args[1], function(results) {console.log('replication complete')});
 
